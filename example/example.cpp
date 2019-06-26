1851b3971aec05c925c43511b91271f8fa0ee03c
tgn3000 <tgntanguangning@gmail.com>
2019-06-27 00:36:59 +0900

    can do class and std::vector<int>

--- example/example.cpp
+++ example/example.cpp
@@ -1,11 +1,41 @@
+#include <vector>
 #include <pybind11/pybind11.h>
+// implicitly conversion to std::vector<int>
+#include <pybind11/stl.h>
 
-int add(int i, int j) {
-    return i + j;
-}
+namespace py = pybind11;
 
-PYBIND11_MODULE(example, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
+// int add(int i, int j) {
+//     return i + j;
+// }
 
-    m.def("add", &add, "A function which adds two numbers");
+struct Foo {
+public:
+    Foo() {}
+    ~Foo() {}
+    void SetVecInt(std::vector<int> a_) { a = std::move(a_); }
+    const std::vector<int>& GetVecInt() const& { return a; }
+    std::vector<int> a;
+};
+
+// PYBIND11_MODULE(example, m) {
+//     m.doc() = "pybind11 example plugin"; // optional module docstring
+
+//     m.def("add", &add, "A function which adds two numbers");
+// }
+
+PYBIND11_MODULE(example, m)
+{
+	m.doc() = "";
+	using namespace py::literals;
+
+	py::class_<Foo>(m, "Foo")
+    .def(py::init<>())
+    .def("SetVecInt", [](Foo &self, const std::vector<int>& list) {
+        self.SetVecInt(list);
+    })
+    .def("GetVecInt", [](const Foo &self) {
+        return self.a;
+    })
+    ;
 }
