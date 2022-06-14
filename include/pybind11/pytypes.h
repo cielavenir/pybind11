d505be0d01a89736fcecda1b2aed7f0464d18d79
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-06-14 14:21:32 -0400

    Use rvalue casting

--- include/pybind11/pytypes.h
+++ include/pybind11/pytypes.h
@@ -2492,7 +2492,7 @@ iterator object_api<D>::end() const {
 }
 template <typename D>
 template <typename T> item_accessor object_api<D>::operator[](T &&key) const {
-    return {derived(), reinterpret_borrow<object>(detail::object_or_cast(std::forward<T>(key)))};
+    return {derived(), detail::object_or_cast(std::forward<T>(key)).template cast<object>()};
  }
 template <typename D>
 obj_attr_accessor object_api<D>::attr(handle key) const {
